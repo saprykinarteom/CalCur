ce7cabecdfb225b5427dbe5f5683af45305337de
saprykinarteom <saprykinarteom@gmail.com>
2020-02-23 21:05:05 +0300

    Bug fix
    Signed-off-by: saprykinarteom <saprykinarteom@gmail.com>

--- src/com/saprykinav/currencycalculator/CalCur.java
+++ src/com/saprykinav/currencycalculator/CalCur.java
@@ -7,7 +7,7 @@ import com.saprykinav.currencycalculator.currency.Rub;
 
 public class CalCur {
     public static void main(String[] args) {
-        // example of calculating an expression "1 $ - 11 ₽ = 51.5 ₽"
+        // example of calculating an expression "1 $ * 2 + 2 € = 265 ₽"
         System.out.println(new Dollar(1).multiply(2).plus(new Euro(2)).calculation().resultInRubToString());
     }
 }
